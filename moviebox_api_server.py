0351448ba2e1420c7235f5fbf9af57df57fbbb94
Rahulsarkarwotchly <sarkarrahulsrs@gmail.com>
2026-08-21 08:13:39 +0530

    Update moviebox_api_server.py

--- moviebox_api_server.py
+++ moviebox_api_server.py
@@ -43,7 +43,7 @@ _allowed_origins = [
     for origin in raw.split(",")
     if origin.strip()
 ]
-_allowed_origins += ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:3001"]
+_allowed_origins += ["http://wotchly.netlify.app", "http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:3001"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=sorted(set(_allowed_origins)),
